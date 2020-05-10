5c524ac879a7b0cc92aa138b220d54c35077a36b
Hazal Sulelioglu <hazalsulelioglu@gmail.com>
2020-05-10 17:15:34 +0300

    universite not aralıkları

---
+++ kac_aldim.js
@@ -0,0 +1,26 @@
+var not = 52
+
+if(not>=90) {
+    console.log ('AA aldınız')
+}
+else if(not>=85){
+    console.log ('BA aldınız')
+}
+else if(not>=80){
+    console.log ('BB aldınız')
+}
+else if(not>=75){
+    console.log ('CB aldınız')
+}
+else if(not>=70) {
+    console.log ('CC aldınız')
+}
+else if(not>=65){
+    console.log ('DC aldınız')
+}
+else if(not>=60){
+    console.log ('DD aldınız')
+}
+else {
+    console.log('Kaldınız')
+}
